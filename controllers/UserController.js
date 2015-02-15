711b30c6305760ef9cf2fad320d79be8f613b322
stumbledog <stumbledog@gmail.com>
2015-02-14 22:27:17 -0500

    Implement use controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,8 @@ exports.createUser = function(req, res, callback){
 	var user = new UserModel();
 	var gold = 100;
 	user.save(function(){
-
+		req.session.user = user._id;
+		res.cookie('user_id', user._id, {maxAge: 10 * 365 * 24 * 60 * 60 * 1000, httpOnly: true });
+		callback();
 	});
 }
